b4d51e7f329932237303d36b6fbb67d4231b89db
Miha Susnik <mihec.susnik@gmail.com>
2015-05-05 10:42:40 +0200

    Added fields.

--- app/Word.php
+++ app/Word.php
@@ -4,6 +4,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    protected $table = 'words';
+    const TYPE_SYNONYM = 1;
+    const TYPE_ANTONYM = 2;
 
+    protected $table   = 'words';
+    protected $appends = array('link');
+
+    public function getLinkAttribute()
+    {
+        return route('show', array('word' => $this->getAttribute('word')));
+    }
+
+    public function synonyms()
+    {
+        return $this->belongsToMany('Betoo\Thesaurus\Word', 'word_relationships', 'wordId', 'linkedWordId')
+                    ->wherePivot('relationship_type', '=', $this::TYPE_SYNONYM)
+                    ->whereNull('deleted_at');
+    }
+
+    public function antonyms()
+    {
+        return $this->belongsToMany('Betoo\Thesaurus\Word', 'word_relationships', 'wordId', 'linkedWordId')
+                    ->wherePivot('relationship_type', '=', $this::TYPE_ANTONYM)
+                    ->whereNull('deleted_at');
+    }
 }
