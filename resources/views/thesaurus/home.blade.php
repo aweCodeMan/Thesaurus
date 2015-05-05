150f44c0c07aa0d92dff800b195ead0bbd1eee0d
Miha Susnik <mihec.susnik@gmail.com>
2015-05-05 10:49:12 +0200

    Updated styles.

--- resources/views/thesaurus/home.blade.php
+++ resources/views/thesaurus/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('title')
-    Tezaver
+   Vse slovenske sopomenke in protipomenke
 @endsection
 
 @section('content')
@@ -13,10 +13,10 @@
                     <div class="jumbotron">
                         <h1>Tezaver</h1>
 
-                        <p class="tagline">Tezaver je orodje, ki vam omogoča iskanje sopomenk in protipomenk vseh
+                        <p class="tagline">Iščite sopomenke in protipomenke vseh
                                            slovenskih besed.</p>
 
-                        <p>Naj vam nikoli več beseda ne ostane na jeziku.</p>
+                        <p>Naj vam beseda nikoli več ne obstane na jeziku.</p>
 
                     </div>
                 </div>
@@ -24,42 +24,64 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container section">
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-sm-offset-3 well">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2">
 
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Poiščite sopomenke ali protipomenke...">
-                      <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">Išči!</button>
-                      </span>
+                <div class="search-bar">
+                    <search-word size="large" url="{{ route('home') }}"></search-word>
                 </div>
-
             </div>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container section">
         <div class="row">
             <div class="col-sm-6">
-                <h2>Dodane sopomenke</h2>
+                @if(count($data['lastSynonyms']) > 0)
+                <h2>Zadnje sopomenke</h2>
+
+                    <ul class="linked-words-list">
+
+                            @foreach($data['lastSynonyms'] as $index => $pair)
+                                @if($index % 2)
+                                <li><a href="{{ route('show', $pair->word->word) }}">{{ $pair->word->word }}</a> <span class="text-muted">-></span> <a href="{{ route('show', $pair->linkedWord->word) }}">{{ $pair->linkedWord->word }}</a> <span class="text-muted">({{ $pair->timeDifference }})</span></li>
+                                @endif
+
+                                @endforeach
+
+                    </ul>
+                @endif
             </div>
 
             <div class="col-sm-6">
-                <h2>Dodane protipomenke</h2>
+                @if(count($data['lastAntonyms']) > 0)
+                    <h2>Zadnje protipomenke</h2>
+
+                    <ul class="linked-words-list">
+
+                        @foreach($data['lastAntonyms'] as $index => $pair)
+                            @if($index % 2)
+                            <li><a href="{{ route('show', $pair->word->word) }}">{{ $pair->word->word }}</a> <span class="text-muted">-></span> <a href="{{ route('show', $pair->linkedWord->word) }}">{{ $pair->linkedWord->word }}</a> <span class="text-muted">({{ $pair->timeDifference }})</span></li>
+                            @endif
+                                @endforeach
+
+                    </ul>
+                @endif
             </div>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container section">
         <div class="row">
             <div class="col-xs-12">
                 <h4>Statistika</h4>
 
                 <ul>
-                    <li><span class="text-muted">Število besed v bazi: </span><b>93752</b></li>
-                    <li><span class="text-muted">Število vseh sopomenk: </span><b>1234</b></li>
-                    <li><span class="text-muted">Število vseh protipomenk: </span><b>1245</b></li>
+                    <li><span class="text-muted">Število besed v bazi: </span><b>93275</b></li>
+                    <li><span class="text-muted">Število vseh sopomenk: </span><b>{{ $data['synonymCount'] }}</b></li>
+                    <li><span class="text-muted">Število vseh protipomenk: </span><b>{{ $data['antonymCount'] }}</b>
+                    </li>
                 </ul>
             </div>
         </div>
