197b72e7439c7922e1e181c6d8eabfc65226edef
Miha Susnik <mihec.susnik@gmail.com>
2015-05-12 13:03:18 +0200

    Updated stylings.

--- public/js/app.js
+++ public/js/app.js
@@ -67,32 +67,6 @@ angular.module('thesaurus')
                  };
              });
 angular.module('thesaurus')
-  .directive('removeLinkedWord', function ()
-             {
-                 return {
-                     restrict:   'E',
-                     scope:      {
-                         word:       '=',
-                         linkedWord: '=',
-                         type:       '@',
-                         url:        '@'
-                     },
-                     controller: function ($scope, DeleteWordModal)
-                     {
-                         $scope.remove = function ()
-                         {
-                             DeleteWordModal.openModal($scope.word, $scope.linkedWord, $scope.type, $scope.url).then(function (success)
-                                                                                                                     {
-                                                                                                                         window.location.reload();
-                                                                                                                     });
-                         }
-                     },
-                     template:   '<span class="glyphicon glyphicon-trash remove-linked-word" ng-click="remove()"></span>'
-                 };
-             })
-;
-
-angular.module('thesaurus')
   .factory('DeleteWordModal', function ($modal)
            {
                var modal = {
@@ -152,6 +126,32 @@ angular.module('thesaurus')
                       $modalInstance.dismiss();
                   };
               });
+
+angular.module('thesaurus')
+  .directive('removeLinkedWord', function ()
+             {
+                 return {
+                     restrict:   'E',
+                     scope:      {
+                         word:       '=',
+                         linkedWord: '=',
+                         type:       '@',
+                         url:        '@'
+                     },
+                     controller: function ($scope, DeleteWordModal)
+                     {
+                         $scope.remove = function ()
+                         {
+                             DeleteWordModal.openModal($scope.word, $scope.linkedWord, $scope.type, $scope.url).then(function (success)
+                                                                                                                     {
+                                                                                                                         window.location.reload();
+                                                                                                                     });
+                         }
+                     },
+                     template:   '<span class="glyphicon glyphicon-trash remove-linked-word" ng-click="remove()"></span>'
+                 };
+             })
+;
 
 /**
  * @ngdoc directive
