c2b4116ddd53dbbab1d492b45abecbd5d8bfbec6
Miha Susnik <mihec.susnik@gmail.com>
2015-05-13 09:48:46 +0200

    Fixed definitions.

--- resources/views/thesaurus/partials/definitions.blade.php
+++ resources/views/thesaurus/partials/definitions.blade.php
@@ -1,5 +1,10 @@
 <ul class="definitions-list">
-    @foreach(explode(':', $definitions, -1) as $definition)
-        <li class="text-muted"><i>{{ $definition }}</i></li>
-    @endforeach
+    @if(count($definitions) == 1)
+        <li class="text-muted"><i>{{ $definitions }}</i></li>
+
+    @else
+        @foreach(explode(':', $definitions, -1) as $definition)
+            <li class="text-muted"><i>{{ $definition }}</i></li>
+        @endforeach
+    @endif
 </ul>
