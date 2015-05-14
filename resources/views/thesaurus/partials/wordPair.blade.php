960ccd3ec933e67b897a17dc1abdab0489f131e1
Miha Susnik <mihec.susnik@gmail.com>
2015-05-14 17:10:58 +0200

    Word fix.

--- resources/views/thesaurus/partials/wordPair.blade.php
+++ resources/views/thesaurus/partials/wordPair.blade.php
@@ -1,4 +1,4 @@
-<li><a href="{{ route('show', $pair->word->word) }}">{{ $pair->word->word }}</a>
+<li><a href="{{ route('show', $pair->word->word) }}">{{ $pair->word->pronunciation }}</a>
     <span class="glyphicon glyphicon-chevron-right"></span>
-    <a href="{{ route('show', $pair->linkedWord->word) }}">{{ $pair->linkedWord->word }}</a>
+    <a href="{{ route('show', $pair->linkedWord->word) }}">{{ $pair->linkedWord->pronunciation }}</a>
     <span class="text-muted">({{ $pair->timeDifference }})</span></li>
